ef820754158544403b726727ede40a663e13159d
sooki88 <sookhee9n@gmail.com>
2024-01-23 16:32:09 +0900

    chore: globals 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <>
+      <div className="relative bg-black-200 p-10 rounded-20">
+        <h1 className="p-8 bg-orange tablet:bg-violet pc:bg-red">Landing</h1>
+      </div>
+      <div className="absolute top-0 left-0 p-4 bg-white border border-green text-green z-modal">안녕</div>
+    </>
+  );
 }
