604523895f76137f4b90bb4a335ae688383ca8a2
남숙희 <131663155+sooki88@users.noreply.github.com>
2024-01-24 12:55:55 +0900

    Chore: tailwind.config.ts에서 screens pc 사이즈 수정 (#2)
    Chore: screens pc 1199px 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,7 +54,7 @@ module.exports = {
     screens: {
       mobile: "375px",
       tablet: "744px",
-      pc: "1099px",
+      pc: "1199px",
     },
     zIndex: {
       DEFAULT: "1",
@@ -68,25 +68,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-// 이전 코드 : 에러나서 교체했습니다.
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//     },
-
-//   },
-//   plugins: [],
-// };
-// export default config;
