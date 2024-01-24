f9726944d2723c611811110460b9e4db5e9895da
sooki88 <sookhee9n@gmail.com>
2024-01-24 12:45:30 +0900

    chore: screens pc 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,25 +68,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-// 이전 코드 : 에러나서 교체했습니다.
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//     },
-
-//   },
-//   plugins: [],
-// };
-// export default config;
