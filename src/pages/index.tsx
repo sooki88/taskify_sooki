c289a89dad2d3ab8098e83377c64391f0a89e25d
sooki88 <sookhee9n@gmail.com>
2024-01-26 09:53:54 +0900

    Feat: Chips 작업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,55 @@
+import { ChipCard, ChipCards, ChipColors, ChipProgress } from "@/components/common/chips";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { getCardDetail, postCard } from "../../mock/instance";
+
 export default function Landing() {
-  return <div>Landing</div>;
+  const [columnId, setColumnId] = useState<number>();
+
+  useEffect(() => {
+    //카드 상세 내용
+    const fetchData = async () => {
+      try {
+        const res = await getCardDetail();
+        setColumnId(res?.columnId);
+        setTags(res?.tags);
+
+        return;
+      } catch (error) {
+        console.error("Failed to fetch card details:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const dashbordId = 2527;
+  const [inputValue, setInputValue] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      setTags([...tags, inputValue.trim()]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKeyDown} />
+      <div>
+        {tags.map((tag, index) => (
+          <ChipCard key={index} tag={tag} index={index} />
+        ))}
+      </div>
+      {/* Landing */}
+
+      {/* <ChipProgress columnId={cardDetails.id} /> */}
+
+      {/* <ChipCards tags={cardDetails.tags} /> */}
+
+      {/* <ChipCards tags={tags} /> */}
+      {/* <ChipColors selectColor={selectColor} setSelectColor={setSelectColor} /> */}
+    </div>
+  );
 }
