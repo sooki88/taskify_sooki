1e283c18cbb1fc255c292f7012c1b21ae650b10b
sooki88 <sookhee9n@gmail.com>
2024-01-27 13:04:56 +0900

    Chore: index 초기화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,3 @@
 export default function Landing() {
-  return (
-    <>
-      <div className="relative bg-black-4B4B p-10 rounded-20">
-        <h1 className="p-8 bg-orange tablet:bg-violet pc:bg-red">Landing</h1>
-      </div>
-      <div className="absolute top-0 left-0 p-4 bg-white border border-green text-green z-modal">안녕</div>
-    </>
-  );
+  return <div>Landing</div>;
 }
